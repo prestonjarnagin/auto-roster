ed1b80b7a1b6878cff7bb739b7f08f9485baa5ed
Preston Jarnagin <preston@homebot.ai>
2022-10-09 22:25:59 -0600

    Add slug generation for teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,13 @@ class Team < ApplicationRecord
   after_create :generate_slug!
   has_many :players
 
+  def generate_slug!
+    until slug
+      possible_slug = SecureRandom.hex(3)
+
+      break if Team.find_by(slug: possible_slug)
+
+      update!(slug: possible_slug)
+    end
+  end
 end
