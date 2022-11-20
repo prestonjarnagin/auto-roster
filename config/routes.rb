fd69f94b77c79fb2e6d869772f3af96ec00b59ff
Preston Jarnagin <prstnjarnagin@gmail.com>
2022-11-20 15:39:00 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,14 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  jsonapi_resources :teams, only: %i[create] do
-    jsonapi_resources :players, only: %i[create]
+  namespace 'teams' do
+    get 'find/:slug', to: 'find#show'
   end
+
+  resources :teams, only: %i[create show] do
+    resources :players, only: %i[create]
+  end
+
+  resources :player_position_preferences, only: %i[create],
+                                          path: 'player-position-preferences'
 end
